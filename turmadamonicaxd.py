df35a5ea98a570ce1ce22ec254d727d8ef695b48
Paulo Carato Apenas <paulohenriquecarato@gmail.com>
2024-05-14 14:32:35 -0300

    did the screen code % ad a plcehldr for background

--- turmadamonicaxd.py
+++ turmadamonicaxd.py
@@ -1,3 +1,27 @@
-#it gives luck
-print('Hello World!!!')
-#more luck
+import pygame
+
+#it will be bad
+
+pygame.init()
+
+    ###screen configs###
+screen = pygame.display.set_mode((1200,650))
+pygame.display.set_caption("famoso pega pega")
+texto = pygame.font.SysFont("Comic_Sans", 20)
+    #paralax
+fundo=pygame.image.load('images/street.png')
+#omg i love comic sans
+
+    ###sprites###
+# playerI=
+
+fps = pygame.time.Clock()
+running=True
+while running:
+    screen.blit(fundo, (0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running=False
+    pygame.display.update()
+    fps.tick(60)
+    #paralax here
